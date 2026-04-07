d78afbda874c71d9ec8ff4158e645d20b722cf91
denilsongv <denilsongv@outlook.com>
2026-04-07 17:44:28 -0300

    Update controle_criative.py

--- controle_criative.py
+++ controle_criative.py
@@ -473,7 +473,7 @@ if menu == "📋 Dashboard":
         with tab1:
             def highlight_pendente(row):
                 if col_forma and row.get(col_forma) == "aguardando pagamento":
-                    return ["background-color: #ffcccc"] * len(row)
+                    return ["background-color: #ffc222"] * len(row)
                 return [""] * len(row)
 
             styled_df = df.style.apply(highlight_pendente, axis=1)
